59ac57e0e9edfc918eb0f914cd788419995428c6
ramqa211-1 <ramqaveles@gmail.com>
2024-02-16 17:42:36 +0200

    sd

--- src/test/java/homeTaskTest/youtubeTaskTest.java
+++ src/test/java/homeTaskTest/youtubeTaskTest.java
@@ -14,13 +14,13 @@ public class youtubeTaskTest extends BaseTest {
             String password="";
                 // Identify elements and perform login actions
                 // Example: Locate username and password fields by resource-id and enter credentials
-                AndroidElement usernameField = (AndroidElement) driver.findElementById("com.facebook.katana:id/login_username");
-                AndroidElement passwordField = (AndroidElement) driver.findElementById("com.facebook.katana:id/login_password");
-                AndroidElement loginButton = (AndroidElement) driver.findElementById("com.facebook.katana:id/login_login");
-
-                usernameField.sendKeys(username);
-                passwordField.sendKeys(password);
-                loginButton.click();
+//                AndroidElement profileBottomIcon = (AndroidElement) driver.findElementByXPath("//*[@id=\"screenshotContainer\"]/div[2]/div/div/div/div/div[116]");
+                AndroidElement menuSide = (AndroidElement) driver.findElementByXPath("//android.widget.FrameLayout[@resource-id=\"com.instagram.android:id/bottom_sheet_camera_container\"]");
+//                AndroidElement loginButton = (AndroidElement) driver.findElementById("com.facebook.katana:id/login_login");
+//
+//                usernameField.sendKeys(username);
+//                passwordField.sendKeys(password);
+//                loginButton.click();
 
                 // Additional actions if needed, e.g., handling login success or failure
             }
