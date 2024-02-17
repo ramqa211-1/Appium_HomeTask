6df27414a3671fb4dbd05ae7f2c654533fd2bda7
ramqa211-1 <ramqaveles@gmail.com>
2024-02-17 14:26:57 +0200

    sd

---
+++ src/main/java/com/bluenile/testkit/locators/InstagramAppLocators.java
@@ -0,0 +1,23 @@
+package com.bluenile.testkit.locators;
+
+import org.openqa.selenium.By;
+
+public class InstagramAppLocators {
+
+    public static By profileButtonBottomMenu = By.id("com.instagram.android:id/tab_avatar");
+    public static By menuSide = By.xpath("//android.widget.Button[@content-desc=\"Options\"]");
+    public static By settingsPrivacyButton = By.xpath("//android.widget.Button[@content-desc=\"Settings and privacy\"]");
+    public static By logoutAllAccounts = By.xpath("//android.widget.Button[@content-desc=\"Log out all accounts. Logs out all accounts\"]");
+    public static By logOut = By.id("com.instagram.android:id/bb_primary_action_container");
+    public static By logInAnotherAccount = By.xpath("//android.widget.Button[@content-desc=\"Log into another account\"]/android.view.ViewGroup");
+    public static By emailField = By.xpath("//android.widget.EditText[@text=\"ramivelestal\"]");
+
+    public static By passWordField = By.xpath("//android.widget.FrameLayout[@resource-id=\"com.instagram.android:id/layout_container_main\"]/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]");
+    public static By logInButton = By.xpath("//android.widget.Button[@content-desc=\"Log in\"]/android.view.ViewGroup");
+    public static By storyHighlightsHeader = By.id("com.instagram.android:id/tray_header");
+
+    public static By errorMessageLogin = By.xpath("//android.widget.LinearLayout[@resource-id=\"android:id/parentPanel\"]");
+
+
+
+}
--- src/test/java/homeTaskTest/maccabiAppTaskTest.java
+++ src/test/java/homeTaskTest/maccabiAppTaskTest.java
@@ -1,18 +1,22 @@
 package homeTaskTest;
 import com.bluenile.testkit.base.BaseTest;
-import com.bluenile.testkit.locators.InstgramAppLocators;
+import com.bluenile.testkit.locators.InstagramAppLocators;
 import io.appium.java_client.android.AndroidElement;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
-import static com.bluenile.testkit.pages.desktop.BasePageObject.*;
+import static com.bluenile.testkit.pages.BasePageObject.*;
 
 
 public class maccabiAppTaskTest extends BaseTest {
 
 
+    @Severity(SeverityLevel.NORMAL)
+    @Description ("check app log in, swipe action , get text. assert text and tap action")
+    @Story("test verification")
 
     @Test(groups = {})
-    public void instgramLoginTest() throws Exception {
-        InstgramAppLocators instgramAppLocators=new InstgramAppLocators();
+    public void instagramLoginTest() throws Exception {
+        InstagramAppLocators instgramAppLocators=new InstagramAppLocators();
         String emailFieldText = "israelIsraeli";
         Thread.sleep(10000);
         AndroidElement profileButtonBottomMenu = (AndroidElement) driver.findElement(instgramAppLocators.profileButtonBottomMenu);
@@ -38,6 +42,43 @@ public class maccabiAppTaskTest extends BaseTest {
         emailField.sendKeys(emailFieldText);
         AndroidElement logInButton = (AndroidElement) driver.findElement(instgramAppLocators.logInButton);
         tapElement(logInButton);
+    }
+
+    @Test(groups = {})
+    public void instagramInValidLoginTest() throws Exception {
+        InstagramAppLocators instgramAppLocators=new InstagramAppLocators();
+        String emailFieldText = "israelIsraeli";
+        String passwordText = "";
+        Thread.sleep(10000);
+        AndroidElement profileButtonBottomMenu = (AndroidElement) driver.findElement(instgramAppLocators.profileButtonBottomMenu);
+        tapElement(profileButtonBottomMenu);
+        AndroidElement storyHighlightsHeader = (AndroidElement) driver.findElement(instgramAppLocators.storyHighlightsHeader);
+        String storyHighlightsHeaderText= getTextFromElement(storyHighlightsHeader);
+        assertContains(storyHighlightsHeaderText,"Story highlights");
+        AndroidElement menuSide = (AndroidElement) driver.findElement(instgramAppLocators.menuSide);
+        tapElement(menuSide);
+        AndroidElement settingsPrivacyButton = (AndroidElement) driver.findElement(instgramAppLocators.settingsPrivacyButton);
+        tapElement(settingsPrivacyButton);
+        swipeDown();
+        swipeDown();
+        AndroidElement logoutAllAccounts = (AndroidElement) driver.findElement(instgramAppLocators.logoutAllAccounts);
+        tapElement(logoutAllAccounts);
+        AndroidElement logOut = (AndroidElement) driver.findElement(instgramAppLocators.logOut);
+        tapElement(logOut);
+        Thread.sleep(10000);
+        AndroidElement logInAnotherAccount = (AndroidElement) driver.findElement(instgramAppLocators.logInAnotherAccount);
+        tapElement(logInAnotherAccount);
+        AndroidElement emailField = (AndroidElement) driver.findElement(instgramAppLocators.emailField);
+        tapElement(emailField);
+        emailField.sendKeys(emailFieldText);
+        AndroidElement passWordField = (AndroidElement) driver.findElement(instgramAppLocators.passWordField);
+        tapElement(passWordField);
+        emailField.sendKeys(passwordText);
+        AndroidElement logInButton = (AndroidElement) driver.findElement(instgramAppLocators.logInButton);
+        tapElement(logInButton);
+        AndroidElement errorMessageLogin = (AndroidElement) driver.findElement(instgramAppLocators.errorMessageLogin);
+        String errorMessageLoginText= getTextFromElement(errorMessageLogin);
+        assertContains(errorMessageLoginText,"Enter your password to log in");
     }
 }
 
